c94797fa4986978add719ed74a266dc2d326895e
SARIKONDA LAKSHMI DEVI <146585693+Lakshmi-Devi1@users.noreply.github.com>
2024-09-16 00:30:40 +0530

    Create characters.java

---
+++ characters.java
@@ -0,0 +1,12 @@
+//characters that are present in the string
+public class characters{
+    public static void main(String[] args) {
+        String string="lakshmi";
+        int count=string.length();
+        System.out.println(count);
+        //conevrt all the letters to the uppercase
+        System.out.println("uppercase letters are:");
+        String Upper=string.toUpperCase();
+        System.out.println(Upper);
+    }
+}
