8b3e573c296ec5a10ebb3ccfadb2981dafab0fcf
SARIKONDA LAKSHMI DEVI <146585693+Lakshmi-Devi1@users.noreply.github.com>
2024-09-16 00:27:39 +0530

    Create string reversal.java

---
+++ string reversal.java
@@ -0,0 +1,17 @@
+public class reversal {
+    //reversing of the string in java
+    //we can use the traditional for loop
+    //we can also use the string biilder format
+    public static void main(String args[]){
+        //"lak"==>"kal"
+        //using traditional for loop
+        String reverse="";
+        String name="lakshmi";
+        for(int i=name.length()-1;i>=0;i--){
+            reverse=reverse+name.charAt(i);
+        }
+        System.out.println(reverse);
+
+    }
+    
+}
