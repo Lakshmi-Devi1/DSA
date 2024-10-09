423ec3e2c79d4476f3526347cae7fd10410f44a2
SARIKONDA LAKSHMI DEVI <146585693+Lakshmi-Devi1@users.noreply.github.com>
2024-10-09 23:43:55 +0530

    Create Function.java

---
+++ Function.java
@@ -0,0 +1,12 @@
+public class Main{
+public static void main(String args[]){
+    int num[]={1,2,3};
+    changes(num);
+    System.out.println(num[0]);
+
+}
+static void changes(int arr[]){
+    arr[0]=8;
+}
+
+}
